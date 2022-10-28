53058466a102ffb8e7a51219714b1014b05488e4
Luiz Carlos <designarttdev@gmail.com>
2022-10-28 11:54:08 -0300

    Ajustado validação de visibilidade do campo.

--- Unit/Principal.pas
+++ Unit/Principal.pas
@@ -357,7 +357,7 @@ end;
 
 procedure TfrPrincipal.chkBancoMaisClick(Sender: TObject);
 begin
-  if chkBancoMais.Enabled then
+  if chkBancoMais.Checked then
   begin
     ConectarBaseDados;
     cbEmpresa.Enabled := True;
