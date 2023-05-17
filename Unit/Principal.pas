e214d87bc2e4c5f35013a6546a8d8c230af5cd76
Luiz Carlos <designarttdev@gmail.com>
2023-05-17 17:12:34 -0300

    Update Principal.pas

--- Unit/Principal.pas
+++ Unit/Principal.pas
@@ -73,43 +73,121 @@ begin
     Exit;
   end;
 
-
-  {$REGION 'MontaScript'}
-
-  memo2.lines.clear;
-  vAux := Trim(Memo1.Lines.Text.Replace(#13#10, '').Replace(';', '')); // Retira o enter da string
-  vAux := Trim(vAux.Replace(')', sLineBreak + ' ) ')); // Dá Enter quando encontrar o ")"
-  vAux := Trim(vAux.Replace('(', ' ( ' + sLineBreak));  // Da enter depois do "("
-  vAux := Trim(vAux.Replace(',', ' , ' + sLineBreak));  // Insere Enter depois da Vírgula
-  vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak)); // Retira o enter e esaço e apenas coloca o Enter
-  vAux := Trim(vAux.Replace('WHERE', sLineBreak + ' WHERE ')); // Faz quebra de linha no where
-  vAux := Trim(vAux.Replace('FROM', sLineBreak + ' FROM ')); // Faz quebra de linha no FROM
-  vAux := Trim(vAux.Replace('AND', sLineBreak + ' AND ')); // Faz quebra de linha no AND
-  vAux := Trim(vAux.Replace('INNER JOIN', sLineBreak + ' INNER JOIN ')); // Faz quebra de linha no INNER JOIN
-  vAux := Trim(vAux.Replace('LEFT JOIN', sLineBreak + ' LEFT JOIN ')); // Faz quebra de linha no LEFT JOIN
-  vAux := Trim(vAux.Replace('RIGHT JOIN', sLineBreak + ' RIGHT JOIN ')); // Faz quebra de linha no RIGHT JOIN
-  vAux := Trim(vAux.Replace('CASE', ' CASE ' + sLineBreak));  // Insere Enter depois do CASE
-  vAux := Trim(vAux.Replace('WHEN', ' WHEN ' + sLineBreak));  // Insere Enter depois do WHEN
-  vAux := Trim(vAux.Replace('ELSE', ' ELSE ' + sLineBreak));  // Insere Enter depois do ELSE
-  vAux := Trim(vAux.Replace('ORDER BY', sLineBreak + ' ORDER BY ')); // Faz quebra de linha no LEFT JOIN
-  vAux := Trim(vAux.Replace('GROUP BY', sLineBreak + ' GROUP BY ')); // Faz quebra de linha no LEFT JOIN
-  vAux := Trim(vAux.Replace('''', '"')); // Troca áspas simples por áspas duplas
-  vAux := Trim(vAux.Replace('  ', ' ')); // Troca áspas simples por áspas duplas
-  memo2.Lines.Add(Trim(vAux));
-  x := 000;
-  for i := 0 to Memo2.Lines.Count - 1 do
+  if Copy(Trim(memo1.Lines.Text), 1, Pos(' ', Trim(memo1.Lines.Text)) - 1) = 'INSERT' then
   begin
-    vTextoLinha := vTextoLinha + '{' + FormatFloat('000', x) + '}  ''' + Trim(memo2.Lines.Strings[i]) + ' '' + ' + sLineBreak;
-    x := x + 1;
+    {$REGION 'Insert'}
+
+      memo2.lines.clear;
+      vAux := Trim(Memo1.Lines.Text.Replace(#13#10, '').Replace(';', '')); // Retira o enter da string
+      vAux := Trim(vAux.Replace(')', sLineBreak + ') ')); // Dá Enter quando encontrar o ")"
+      vAux := Trim(vAux.Replace('(', '(' + sLineBreak));  // Da enter depois do "("
+      vAux := Trim(vAux.Replace(',', ',' + sLineBreak));  // Insere Enter depois da Vírgula
+      vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak)); // Retira o enter e esaço e apenas coloca o Enter
+      vAux := Trim(vAux.Replace('''', '"')); // Troca áspas simples por áspas duplas
+      vAux := Trim(vAux.Replace('CASE', 'CASE' + sLineBreak));  // Insere Enter depois do CASE
+      vAux := Trim(vAux.Replace('WHEN', sLineBreak + '  WHEN'));  // Insere Enter antes do WHEN
+      vAux := Trim(vAux.Replace('ELSE', sLineBreak + '  ELSE'));  // Insere Enter antes do ELSE
+      memo2.Lines.Add(Trim(vAux));
+      vRecomeca := False;
+      x := 000;
+      for i := 0 to Memo2.Lines.Count -1 do
+      begin
+        if (not (Pos('(', memo2.Lines.Strings[i]) <> 0) and not(Pos(')', memo2.Lines.Strings[i]) <> 0)) then
+        begin
+          vTextoLinha := vTextoLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+        end
+        else if (Pos('VALUES', memo2.Lines.Strings[i]) <> 0) then
+        begin
+          x := 000;
+          vTextoLinha := vTextoLinha + sLineBreak + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+          vRecomeca := True;
+        end
+        else
+          vTextoLinha := vTextoLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+        x := X + 1;
+      end;
+
+      memo2.Lines.Clear;
+      memo2.Lines.Add(Trim(Copy(Trim(vTextoLinha), 1, Length(Trim(vTextoLinha)) - 1).Replace('"', '''' + '''')) + ';');
+      Memo2.SelectAll;
+      memo2.CopyToClipboard;
+      ShowMessage('Copiado!');
+
+    {$ENDREGION}
+  end
+  else if Copy(Trim(memo1.Lines.Text), 1, Pos(' ', Trim(memo1.Lines.Text)) - 1) = 'UPDATE' then
+  begin
+    {$REGION 'Update'}
+
+      memo2.lines.clear;
+      vAux := Trim(Memo1.Lines.Text.Replace(#13#10, '').Replace(';', '')); // Retira o enter da string
+      vAux := Trim(vAux.Replace(')', sLineBreak + ') ')); // Dá Enter quando encontrar o ")"
+      vAux := Trim(vAux.Replace('(', '(' + sLineBreak));  // Da enter depois do "("
+      vAux := Trim(vAux.Replace(',', ',' + sLineBreak));  // Insere Enter depois da Vírgula
+      vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak)); // Retira o enter e esaço e apenas coloca o Enter
+      vAux := Trim(vAux.Replace('WHERE', sLineBreak + 'WHERE ')); // Faz quebra de linha no where
+      vAux := Trim(vAux.Replace('FROM ', sLineBreak + 'FROM ')); // Faz quebra de linha no FROM
+      vAux := Trim(vAux.Replace('AND ', sLineBreak + 'AND ')); // Faz quebra de linha no AND
+      vAux := Trim(vAux.Replace('INNER JOIN ', sLineBreak + 'INNER JOIN ')); // Faz quebra de linha no INNER JOIN
+      vAux := Trim(vAux.Replace('LEFT JOIN ', sLineBreak + 'LEFT JOIN ')); // Faz quebra de linha no LEFT JOIN
+      vAux := Trim(vAux.Replace('RIGHT JOIN ', sLineBreak + 'RIGHT JOIN ')); // Faz quebra de linha no RIGHT JOIN
+      vAux := Trim(vAux.Replace('CASE', 'CASE' + sLineBreak));  // Insere Enter depois do CASE
+      vAux := Trim(vAux.Replace('WHEN', sLineBreak + '  WHEN'));  // Insere Enter antes do WHEN
+      vAux := Trim(vAux.Replace('ELSE', sLineBreak + '  ELSE'));  // Insere Enter antes do ELSE
+      vAux := Trim(vAux.Replace('''', '"')); // Troca áspas simples por áspas duplas
+      memo2.Lines.Add(Trim(vAux));
+      x := 000;
+      for i := 0 to Memo2.Lines.Count -1 do
+      begin
+        vTextoLinha := vTextoLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+        x := X + 1;
+      end;
+
+      memo2.Lines.Clear;
+      memo2.Lines.Add(Trim(Copy(Trim(vTextoLinha), 1, Length(Trim(vTextoLinha)) - 1).Replace('"', '''' + '''')) + ';');
+      Memo2.SelectAll;
+      memo2.CopyToClipboard;
+      ShowMessage('Copiado!');
+
+    {$ENDREGION}
+  end
+  else if (Copy(Trim(memo1.Lines.Text), 1, Pos(' ', Trim(memo1.Lines.Text)) - 1) = 'SELECT') or (Copy(Trim(memo1.Lines.Text), 1, Pos(#$D#$A, Trim(memo1.Lines.Text)) - 1) = 'SELECT') then
+  begin
+    {$REGION 'Select'}
+
+      memo2.lines.clear;
+      vAux := Trim(Memo1.Lines.Text.Replace(#13#10, '').Replace(';', '')); // Retira o enter da string
+      vAux := Trim(vAux.Replace(')', sLineBreak + ') ')); // Dá Enter quando encontrar o ")"
+  //    vAux := Trim(vAux.Replace('(', '(' + sLineBreak));  // Da enter depois do "("
+      vAux := Trim(vAux.Replace(',', ',' + sLineBreak));  // Insere Enter depois da Vírgula
+      vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak)); // Retira o enter e esaço e apenas coloca o Enter
+      vAux := Trim(vAux.Replace('WHERE', sLineBreak + ' WHERE ')); // Faz quebra de linha no where
+      vAux := Trim(vAux.Replace('FROM ', sLineBreak + ' FROM ')); // Faz quebra de linha no FROM
+      vAux := Trim(vAux.Replace('AND', sLineBreak + ' AND ')); // Faz quebra de linha no AND
+      vAux := Trim(vAux.Replace('INNER JOIN ', sLineBreak + ' INNER JOIN ')); // Faz quebra de linha no INNER JOIN
+      vAux := Trim(vAux.Replace('LEFT JOIN ', sLineBreak + ' LEFT JOIN ')); // Faz quebra de linha no LEFT JOIN
+      vAux := Trim(vAux.Replace('RIGHT JOIN ', sLineBreak + ' RIGHT JOIN ')); // Faz quebra de linha no RIGHT JOIN
+      vAux := Trim(vAux.Replace('CASE', ' CASE ' + sLineBreak));  // Insere Enter depois do CASE
+      vAux := Trim(vAux.Replace('WHEN', ' WHEN '+ sLineBreak));  // Insere Enter depois do WHEN
+      vAux := Trim(vAux.Replace('ELSE', ' ELSE '+ sLineBreak));  // Insere Enter depois do ELSE
+      vAux := Trim(vAux.Replace('ORDER BY', sLineBreak + ' ORDER BY ')); // Faz quebra de linha no LEFT JOIN
+      vAux := Trim(vAux.Replace('''', '"')); // Troca áspas simples por áspas duplas
+      memo2.Lines.Add(Trim(vAux));
+      x := 000;
+      for i := 0 to Memo2.Lines.Count -1 do
+      begin
+        vTextoLinha := vTextoLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+        x := X + 1;
+      end;
+
+      memo2.Lines.Clear;
+      memo2.Lines.Add(Trim(Copy(Trim(vTextoLinha), 1, Length(Trim(vTextoLinha)) - 1).Replace('"', '''' + '''')) + ';');
+      Memo2.SelectAll;
+      memo2.CopyToClipboard;
+      ShowMessage('Copiado!');
+
+    {$ENDREGION}
   end;
-
-  memo2.Lines.Clear;
-  memo2.Lines.Add(Trim(Copy(Trim(vTextoLinha), 1, Length(Trim(vTextoLinha)) - 1).Replace('"', '''' + '''')) + ';');
-  Memo2.SelectAll;
-  memo2.CopyToClipboard;
-  ShowMessage('Copiado!');
-
-  {$ENDREGION}
 
   x := 1;
   Memo3.Lines.Clear;
@@ -240,19 +318,19 @@ var
   vQueryAux : TFDQuery;
 begin
   vQueryAux := TFDQuery.Create(nil);
-  vQuery.Connection := AdcBaseDados;
+  vQueryAux.Connection := AdcBaseDados;
   try
-    vQuery.Close;
-    vQuery.SQL.Clear;
-    vQuery.SQL.Add('SELECT E.CODIGOEMPRESA, E.ALIAS, E.NOMEEMPRESA FROM DGLOB000 E');
-    vQuery.SQL.Add('WHERE E.CODIGOEMPRESA = :CODIGOEMPRESA;');
+    vQueryAux.Close;
+    vQueryAux.SQL.Clear;
+    vQueryAux.SQL.Add('SELECT E.CODIGOEMPRESA, E.ALIAS, E.NOMEEMPRESA FROM DGLOB000 E');
+    vQueryAux.SQL.Add('WHERE E.CODIGOEMPRESA = :CODIGOEMPRESA;');
 
-    vQuery.ParamByName('CODIGOEMPRESA').AsInteger := StrToInt(vCodigoEmpresa);
-    vQuery.Open;
-    vQuery.FetchAll;
-    vQuery.First;
+    vQueryAux.ParamByName('CODIGOEMPRESA').AsInteger := StrToInt(vCodigoEmpresa);
+    vQueryAux.Open;
+    vQueryAux.FetchAll;
+    vQueryAux.First;
 
-    Result := vQuery.FieldByName('ALIAS').AsString;
+    Result := vQueryAux.FieldByName('ALIAS').AsString;
   finally
     FreeAndNil(vQueryAux);
   end;
@@ -347,19 +425,17 @@ var
   aIni : TIniFile;
   I : Integer;
 begin
-//  try
-//    aIni := TIniFile.Create(ExtractFilePath(Application.ExeName) + '\acessobanco.ini');
-//    vUserName := Trim(aIni.ReadString('ACESSO', 'usuario', ''));
-//    vPassword := Trim(aIni.ReadString('ACESSO', 'password', ''));
-//  finally
-//    aIni.Free;
-//  end;
+  try
+    aIni := TIniFile.Create(ExtractFilePath(Application.ExeName) + '\acessobanco.ini');
+    vUserName := Trim(aIni.ReadString('ACESSO', 'usuario', ''));
+    vPassword := Trim(aIni.ReadString('ACESSO', 'password', ''));
+  finally
+    aIni.Free;
+  end;
 
   try
     aIni := TIniFile.Create(ExtractFileDrive(Application.ExeName) + '\Sge32\alias.ini');
     vBaseDados := Trim(aIni.ReadString('BASEDADOS', 'database', EmptyStr));
-    vUserName  := 'SYSDBA';
-    vPassword  := Trim(aIni.ReadString('BASEDADOS', 'senha', ''));
     if vBaseDados <> '' then
     begin
       try
@@ -384,9 +460,7 @@ begin
         for I := 0 to vQuery.RecordCount - 1 do
         begin
           if Trim(aIni.ReadString(vQuery.FieldByName('ALIAS').AsString, 'database', EmptyStr)) <> '' then
-          begin
             cbEmpresa.Items.Add(Trim(vQuery.FieldByName('CODIGOEMPRESA').AsInteger.ToString + ' - ' + vQuery.FieldByName('NOMEEMPRESA').AsString));
-          end;
           vQuery.Next;
         end;
 
